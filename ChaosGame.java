f7260a7f274c4ab17888a2b2162f65908b26df7f
bmatejek <bartosz.matejek@bluesoft.com>
2019-06-17 13:10:48 +0200

    checking if point is in triangle

--- ChaosGame.java
+++ ChaosGame.java
@@ -3,6 +3,40 @@ import java.awt.*;
 import java.util.Random;
 
 public class ChaosGame extends JPanel {
+    class Triangle {
+        Triangle(double x1, double y1, double x2, double y2, double x3,
+                 double y3) {
+            this.x3 = x3;
+            this.y3 = y3;
+            y23 = y2 - y3;
+            x32 = x3 - x2;
+            y31 = y3 - y1;
+            x13 = x1 - x3;
+            det = y23 * x13 - x32 * y31;
+            minD = Math.min(det, 0);
+            maxD = Math.max(det, 0);
+        }
+
+        boolean contains(double x, double y) {
+            double dx = x - x3;
+            double dy = y - y3;
+            double a = y23 * dx + x32 * dy;
+            if (a < minD || a > maxD)
+                return false;
+            double b = y31 * dx + x13 * dy;
+            if (b < minD || b > maxD)
+                return false;
+            double c = det - a - b;
+            if (c < minD || c > maxD)
+                return false;
+            return true;
+        }
+
+        private final double x3, y3;
+        private final double y23, x32, y31, x13;
+        private final double det, minD, maxD;
+    }
+
     private int maxX;
     private int maxY;
     private int ax;
@@ -30,12 +64,20 @@ public class ChaosGame extends JPanel {
         g2d.drawLine(cx, cy, cx, cy);
         System.out.println(getWidth());
         System.out.println(getHeight());
-        lastX = random.nextInt(maxX);
-        lastY = random.nextInt(maxY);
+        boolean shouldGenerateValueAgain = true;
+        Triangle triangle = new Triangle(ax, ay, bx, by, cx, cy);
+        while (shouldGenerateValueAgain) {
+            lastX = random.nextInt(maxX);
+            lastY = random.nextInt(maxY);
+            if (triangle.contains(lastX, lastY)) {
+                shouldGenerateValueAgain = false;
+            }
+        }
 
         g2d.setColor(Color.pink);
+        g2d.setStroke(new BasicStroke((float) 8));
         g2d.drawLine(lastX, lastY, lastX, lastY);
-
+        g2d.setStroke(new BasicStroke((float) 1));
         for (int i = 0; i <= 1000000; i++) {
             if (i < 50) {
                 continue;
